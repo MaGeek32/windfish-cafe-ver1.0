211fed7a76281bcf168267ac5350150e53ab7e53
MaGeek32 <sfen0011@gmail.com>
2022-07-04 15:12:34 +1000

    Empty instruction added

---
+++ components/StoryOutput/emptyStory.js
@@ -0,0 +1,22 @@
+import { View, StyleSheet } from "react-native"
+import { GlobalStyles } from "../../constants/styles"
+
+
+
+function emptyStory () {
+  return (
+    <View style={styles.container}>
+      <Text>Start your first story inspiration by clicking coffee icon in the right corner~</Text>
+    </View>
+  )
+}
+
+export default emptyStory
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    flex: 1,
+    backgroundColor: GlobalStyles.colors.primary700,
+  }
+})
--- screens/StoryList.js
+++ screens/StoryList.js
@@ -1,10 +1,12 @@
 import { useContext, useEffect, useState } from 'react'
-
+import { View, Text, StyleSheet } from 'react-native'
 import StoryOutput from '../components/StoryOutput/StoryOutput'
 import ErrorOverlay from '../components/UI/ErrorOverlay'
 import LoadingOverlay from '../components/UI/LoadingOverlay'
 import { StoriesContext } from '../store/stories-context'
 import { fetchStories } from '../util/localStorage'
+import { GlobalStyles } from "../constants/styles"
+
 
 function StoryList () {
   const [isFetching, setIsFetching] = useState(true)
@@ -28,6 +30,11 @@ function StoryList () {
     getStories()
   }, [])
 
+  // function isStart (item) {
+  //   if (item = []) { return true }
+  //   else { return false }
+  // }
+
   if (error && !isFetching) {
     console.log(error)
     return <ErrorOverlay message={error} />
@@ -38,14 +45,46 @@ function StoryList () {
   }
 
   const getStories = storiesCtx.stories
+  // console.log(getStories)
+  // console.log(isStart(getStories))
   // console.log(storiesCtx.stories)
+  if (getStories.length === 0) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Start your first story inspiration by clicking coffee icon in the right corner!</Text>
+      </View>
+    )
+  }
 
-  return (
-    <StoryOutput
-      stories={getStories}
+  else {
+    return (
+      <StoryOutput
+        stories={getStories}
 
-    />
-  )
+      />
+    )
+
+  }
+
 }
 
 export default StoryList
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    flex: 1,
+    backgroundColor: GlobalStyles.colors.primary700,
+    justifyContent: 'center',
+
+
+  },
+  text: {
+    fontSize: 20,
+    color: GlobalStyles.colors.primary600,
+    textAlign: 'center',
+    margin: 8,
+
+
+  }
+})
